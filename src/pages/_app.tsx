e82ab40187e2393e0c5d45b1ac1c82ba05ec5332
Sato-07 <nivellesachraf@gmail.com>
2024-09-13 12:04:31 +0200

    feat: create reusable Modal component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { Layout } from "@/components/Layout";
+import { Modal } from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { LayoutRouter } from "next/dist/server/app-render/entry-base";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <Layout>
+    <Modal actionLabel="Submit" isOpen title="Test Modal" />
     <Component {...pageProps} />
   </Layout>
   );
